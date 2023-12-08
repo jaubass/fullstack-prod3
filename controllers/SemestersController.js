efe58208881654788b080b0de70c0dfc00bc370d
xavi-mat <1102677+xavi-mat@users.noreply.github.com>
2023-12-08 11:39:42 +0100

    add mutations in controllers

--- controllers/SemestersController.js
+++ controllers/SemestersController.js
@@ -8,6 +8,15 @@ const SemestersController = {
         return await Semester.findById(id)
             .populate("subjects");
     },
+    createSemester: async newSemester => {
+        return await Semester.create(newSemester);
+    },
+    updateSemester: async (id, updatedSemester) => {
+        return await Semester.findByIdAndUpdate(id, updatedSemester, {new: true});
+    },
+    deleteSemester: async id => {
+        return await Semester.findByIdAndDelete(id);
+    },
 };
 
 
--- controllers/SubjectsController.js
+++ controllers/SubjectsController.js
@@ -1,4 +1,5 @@
 import Subject from "../models/Subject.js";
+import Semester from "../models/Semester.js";
 
 const SubjectsController = {
     getSubjectById: async id => {
@@ -6,6 +7,21 @@ const SubjectsController = {
     },
     getSubjectsBySemesterId: async semId => {
         return await Subject.find({ semId });
+    },
+    createSubject: async newSubject => {
+        const subject = await Subject.create(newSubject);
+        // Add subject to its semester
+        await Semester.findByIdAndUpdate(
+            newSubject.semId,
+            { $push: { subjects: subject._id } }
+        );
+        return subject;
+    },
+    updateSubject: async (id, updatedSubject) => {
+        return await Subject.findByIdAndUpdate(id, updatedSubject, {new: true});
+    },
+    deleteSubject: async id => {
+        return Subject.findByIdAndDelete(id);
     },
 };
 
